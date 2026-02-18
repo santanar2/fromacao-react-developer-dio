aafe3b080cfd1649b39317c297aa28a6310aa685
santanar2 <santana.r2d2@gmail.com>
2026-02-18 12:49:01 -0300

    ajustado  o logo  nome login descrição

--- 5-CRIANDO PROJETO COM REACT CLI/gitfind/src/pages/home/index.js
+++ 5-CRIANDO PROJETO COM REACT CLI/gitfind/src/pages/home/index.js
@@ -1,26 +1,37 @@
 import Header from "../../components/header";
 import "./style.css";
+import githubLogo from "../../assets/github.png";  // ← IMPORT CORRETO
 
 function Home() {
-  const background = require("../../assets/github.png").default || require("../../assets/github.png");
-
   return (
     <div className="App">
       <Header />
 
       <div className="conteudo">
-        {background && (
-          <img src={background} alt="github" className="github-logo" />
-        )}
+        <img src={githubLogo} alt="github" className="github-logo" />
+        
         <div className="background">
-          
           <div>
             <input name="usuario" placeholder="@username" />
-            <button>Buscas</button>
+            <button>Buscar</button>
           </div>
-        
+          
+          <div className="perfil">
+            <img 
+              src="https://avatars.githubusercontent.com/u/167146512?v=4" 
+              className="profile"
+              alt="foto"  // ← alt CORRETO aqui
+            /> 
+            <div className="perfil-info">
+              <h3>Ricardo Honório de Santana</h3>
+              <span>@santanar2</span>
+              <p>Descrição</p>
+            </div>   
+          </div> 
+          <hr className="li"/>
         </div>
-        <div className="info">eu</div>
+        
+        <div className="info"></div>
       </div>
     </div>
   );
