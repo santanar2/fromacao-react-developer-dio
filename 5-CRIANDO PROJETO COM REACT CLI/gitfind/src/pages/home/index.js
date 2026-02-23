88628a5ff31c4a8b40b6dc60481325e2f2aeda9b
santanar2 <santana.r2d2@gmail.com>
2026-02-23 13:35:42 -0300

    criado repositoris

--- 5-CRIANDO PROJETO COM REACT CLI/gitfind/src/pages/home/index.js
+++ 5-CRIANDO PROJETO COM REACT CLI/gitfind/src/pages/home/index.js
@@ -1,38 +1,45 @@
 import Header from "../../components/header";
-import "./style.css";
-import githubLogo from "../../assets/github.png";  // ← IMPORT CORRETO
+import ItemList from "../../components/ItemList";
+import githubLogo from "../../assets/github.png";
+import "./style.css"; // CSS da página Home, se houver
 
 function Home() {
   return (
-    <div className="App">
+    <div className="container">
       <Header />
 
-      <div className="conteudo">
-        <img src={githubLogo} alt="github" className="github-logo" />
-        
-        <div className="background">
-          <div>
-            <input name="usuario" placeholder="@username" />
-            <button>Buscar</button>
-          </div>
-          
-          <div className="perfil">
-            <img 
-              src="https://avatars.githubusercontent.com/u/167146512?v=4" 
-              className="profile"
-              alt="foto"  // ← alt CORRETO aqui
-            /> 
-            <div className="perfil-info">
-              <h3>Ricardo Honório de Santana</h3>
-              <span>@santanar2</span>
-              <p>Descrição</p>
-            </div>   
-          </div> 
-          <hr className="li"/>
+      <div className="content">
+
+        <div className="search">
+          <input name="usuario" placeholder="@username" />
+          <button>Buscar</button>
         </div>
-        
-        <div className="info"></div>
+
+        <div className="profile">
+          <img 
+            src="https://avatars.githubusercontent.com/u/167146512?v=4"
+            className="profile-img"
+            alt="profile"
+          />
+
+          <div>
+            <h3>Ricardo Honório de Santana</h3>
+            <span>@santanar2</span>
+            <p>Descrição</p>
+          </div>
+        </div>
+
+        <hr />
+
+        <h4>Repositórios</h4>
+
+        <ItemList title="teste1" description="descrição teste1" />
+        <ItemList title="teste2" description="descrição teste2" />
+        <ItemList title="teste3" description="descrição teste3" />
+
       </div>
+
+      <img src={githubLogo} alt="github" className="background-logo"/>
     </div>
   );
 }
