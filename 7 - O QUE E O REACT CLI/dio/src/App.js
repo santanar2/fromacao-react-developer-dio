d3d3a2e1991ad768844798dafab4f99a66b4085a
santanar2 <santana.r2d2@gmail.com>
2026-03-09 13:22:22 -0300

    projeto react cli add navegacao no projeto

---
+++ 7 - O QUE E O REACT CLI/dio/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./home";
+import Login from "./login";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
