ebf0fceadbd4f1847bcea2e7c0b19557fe4bc669
santanar2 <santana.r2d2@gmail.com>
2026-03-12 11:45:39 -0300

    criando tela de login

--- 8 - ESTILIZACAO COM STYLED-COMPONENTS/dio-clone/src/App.js
+++ 8 - ESTILIZACAO COM STYLED-COMPONENTS/dio-clone/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 
 function App() {
   return (
--- 8 - ESTILIZACAO COM STYLED-COMPONENTS/dio-clone/src/pages/login/index.jsx
+++ 8 - ESTILIZACAO COM STYLED-COMPONENTS/dio-clone/src/pages/login/index.jsx
@@ -1,10 +1,57 @@
-const Login = () => {
+import { MdEmail, MdLock } from "react-icons/md";
+
+import Button from "../../components/Button";
+import Header from "../../components/Header";
+import Input from "../../components/Input";
+
+import {
+  Column,
+  Container,
+  CriarText,
+  EsqueciText,
+  Row,
+  SubTitleLogin,
+  Title,
+  TitleLogin,
+  Wrapper
+} from "./styles";
+
+function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-      <p>Página de login</p>
-    </div>
+    <>
+      <Header />
+
+      <Container>
+        <Column>
+          <Title>
+            A plataforma para você aprender com experts, dominar as principais
+            tecnologias e entrar mais rápido nas empresas mais desejadas.
+          </Title>
+        </Column>
+
+        <Column>
+          <Wrapper>
+            <TitleLogin>Faça seu cadastro</TitleLogin>
+            <SubTitleLogin>
+              Faça seu login e make the change._
+            </SubTitleLogin>
+          </Wrapper>
+
+          <form>
+            <Input placeholder="E-mail" leftIcon={<MdEmail />} />
+            <Input placeholder="Senha" type="password" leftIcon={<MdLock />} />
+            <Button title="Entrar" variant="secondary" type="submit" />
+          </form>
+
+          <Row>
+            <EsqueciText>Esqueci minha senha</EsqueciText>
+            <CriarText>Criar conta</CriarText>
+          </Row>
+
+        </Column>
+      </Container>
+    </>
   );
-};
+}
 
 export default Login;
