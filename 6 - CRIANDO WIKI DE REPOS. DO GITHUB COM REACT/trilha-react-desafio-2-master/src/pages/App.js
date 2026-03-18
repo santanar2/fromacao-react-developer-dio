40f5b1aca405046d1fb2156f236956e8e6faaa33
santanar2 <santana.r2d2@gmail.com>
2026-03-18 11:39:28 -0300

    criando tela de usuario logado e integração entre páginas

--- 6 - CRIANDO WIKI DE REPOS. DO GITHUB COM REACT/trilha-react-desafio-2-master/src/pages/App.js
+++ 6 - CRIANDO WIKI DE REPOS. DO GITHUB COM REACT/trilha-react-desafio-2-master/src/pages/App.js
@@ -1,11 +1,9 @@
-
 import { useState } from 'react';
 import gitLogo from '../assets/github.png'
 import Input from '../components/Input';
 import Button from '../components/Button';
 import ItemRepo from '../components/ItemRepo';
 import { api } from '../services/api';
-
 import { Container } from './styles';
 
 function App() {
@@ -13,39 +11,53 @@ function App() {
   const [currentRepo, setCurrentRepo] = useState('');
   const [repos, setRepos] = useState([]);
 
-
   const handleSearchRepo = async () => {
+  console.log("Buscando:", currentRepo);
 
-    const {data} = await api.get(`repos/${currentRepo}`)
+  try {
+    const { data } = await api.get(`repos/${currentRepo}`);
+    console.log("Resposta da API:", data);
 
-    if(data.id){
-
+    if (data.id) {
       const isExist = repos.find(repo => repo.id === data.id);
 
-      if(!isExist){
+      if (!isExist) {
         setRepos(prev => [...prev, data]);
-        setCurrentRepo('')
-        return
+        setCurrentRepo('');
       }
-
     }
-    alert('Repositório não encontrado')
 
+  } catch (error) {
+    console.log("Erro:", error.response);
+    alert('Repositório não encontrado');
   }
+};
+
 
   const handleRemoveRepo = (id) => {
-    console.log('Removendo registro', id);
-
-    // utilizar filter.
-  }
-
+    const filtered = repos.filter(repo => repo.id !== id);
+    setRepos(filtered);
+  };
 
   return (
     <Container>
       <img src={gitLogo} width={72} height={72} alt="github logo"/>
-      <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
+
+      <Input 
+        value={currentRepo}
+        onChange={(e) => setCurrentRepo(e.target.value)}
+      />
+
       <Button onClick={handleSearchRepo}/>
-      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo}/>)}
+
+      {repos.map(repo => (
+        <ItemRepo
+          key={repo.id}
+          handleRemoveRepo={handleRemoveRepo}
+          repo={repo}
+        />
+      ))}
+
     </Container>
   );
 }
